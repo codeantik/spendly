ccc1036c923ae2f16f83cd9479111c02387d0f5d
codeantik <ankitsinankitsin888000@gmail.com>
2026-06-03 18:08:33 +0530

    added demo video button

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,7 @@
 // main.js — students will add JavaScript here as features are built
+
+const VIDEO_URL = "https://www.youtube.com/watch?v=YOUR_VIDEO_ID";
+
+function openDemo() {
+    window.open(VIDEO_URL, "_blank", "noopener,noreferrer");
+}
